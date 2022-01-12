e19b551189d346cedd8e1d2b71d1347c6409fb49
allaineg <allaineg@surecomply.co>
2022-01-12 10:03:45 +0800

    Initial Commit
    Initial Commit

---
+++ ALTER SEQUENCE REPLACE/frmGenerateReSeed.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace ALTER_SEQUENCE_REPLACE
+{
+    partial class frmGenerateReSeed
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTable = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPK = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtTable
+            // 
+            this.txtTable.Location = new System.Drawing.Point(13, 29);
+            this.txtTable.Multiline = true;
+            this.txtTable.Name = "txtTable";
+            this.txtTable.Size = new System.Drawing.Size(240, 409);
+            this.txtTable.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Table";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(271, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Primary Key";
+            // 
+            // txtPK
+            // 
+            this.txtPK.Location = new System.Drawing.Point(274, 29);
+            this.txtPK.Multiline = true;
+            this.txtPK.Name = "txtPK";
+            this.txtPK.Size = new System.Drawing.Size(240, 409);
+            this.txtPK.TabIndex = 3;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(702, 444);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 4;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(537, 29);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(240, 409);
+            this.txtResult.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(537, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Result";
+            // 
+            // frmGenerateReSeed
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(794, 473);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.txtPK);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTable);
+            this.Name = "frmGenerateReSeed";
+            this.Text = "Generate Re-seed Script";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTable;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPK;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ALTER SEQUENCE REPLACE/frmGenerateReSeed.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ALTER_SEQUENCE_REPLACE
+{
+    public partial class frmGenerateReSeed : Form
+    {
+        public frmGenerateReSeed()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            txtResult.Text = string.Empty;
+
+            StringBuilder sb = new StringBuilder();
+                       
+            sb.AppendLine("raise notice 'UPDATE ThisTable';");
+            sb.AppendLine("select max(\"ThisColumn\") from public.\"ThisTable\" into maxid;");
+            sb.AppendLine("if (maxid is not null) THEN");
+            sb.AppendLine("execute 'alter SEQUENCE public.\"ThisTable_ThisColumn_seq\" RESTART with '|| maxid+1;");
+            sb.AppendLine("raise notice 'SUCCESSFULLY UPDATED Sequence with ThisColumn: %', maxid;");
+            sb.AppendLine("end if;");
+            sb.AppendLine("if (maxid is null) THEN");
+            sb.AppendLine("execute 'alter SEQUENCE public.\"ThisTable_ThisColumn_seq\" RESTART with '|| 1;");
+            sb.AppendLine("raise notice 'SUCCESSFULLY UPDATED Sequence with ThisColumn: 1';");
+            sb.AppendLine("end if;");
+
+            var textToUpdate = sb.ToString();
+
+            string scriptResult = string.Empty;
+
+            StringBuilder sb1 = new StringBuilder();
+
+            sb1.AppendLine("DO $$DECLARE maxid int;");
+            sb1.AppendLine("BEGIN\n");
+
+            scriptResult = sb1.ToString();
+
+            for (int i = 0; i < txtTable.Lines.Count(); i++)
+            {
+                scriptResult = scriptResult + "\n" + textToUpdate.Replace("ThisTable", txtTable.Lines[i]).Replace("ThisColumn", txtPK.Lines[i]);
+            }
+            
+            StringBuilder sb2 = new StringBuilder();
+
+            sb2.Append(scriptResult + "\n");
+            sb2.AppendLine("END");
+            sb2.AppendLine("$$;");
+            
+            txtResult.Text = sb2.ToString();
+        }
+    }
+}
